7f01cf2a0011a324b51f6ba35f0f25b9a1135d76
NASDAMI Quatadah <Quatadah.Nasdami@bordeaux-inp.fr>
2020-12-16 09:09:14 +0100

    end of feuille2.c

--- feuille2.c
+++ feuille2.c
@@ -67,6 +67,57 @@ int is_in_alphabet(char c)
     return ((c >= 65 && c<= 90) || (c >= 97 && c<=122 )); 
 }
 
+char lower(char c)
+{
+    if (c>=65 && c<=90)
+        return c + 32;
+    else if (c >= 97 && c<= 122)
+        return c;
+    else 
+        return 0;
+}
+
+char* mirror(char s[])
+{
+    int len = strlen(s);
+    int mid = len/2;
+    for(int i =0;i<mid ; i++)
+    {
+        char tmp = s[i];
+        s[i] = s[len-i-1];
+        s[len-i-1] = tmp;
+    }
+    return s;
+}
+
+int count_int(int n)
+{
+    int sum = 0;
+    while (n>1)
+    {
+        n /= 10;
+        sum++;
+    }
+    return sum;
+}
+
+void itoa(int n, char *str)
+{
+    int bool = n<0;
+    if (bool)
+        n = -n;
+    
+    int i = 0;
+    do{
+        str[i] = (n%10) + '0';
+        n /= 10;
+        i++;
+    }while(n);
+
+    str[i] = 0;
+    mirror(str);
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -77,8 +128,16 @@ int main(int argc, char *argv[])
 
     //printf("This word is a palindrome ? %s -> %d.\n",argv[1],is_palindrome(argv[1]));
 
-    printf("This character is an alphabet ? %c -> %d\n",argv[1][0],is_in_alphabet(argv[1][0]));
-    printf("This character is an alphabet ? %c -> %d\n",argv[1][0],isalpha(argv[1][0]));
+    //printf("This character is an alphabet ? %c -> %d\n",argv[1][0],is_in_alphabet(argv[1][0]));
+    //printf("This character is an alphabet ? %c -> %d\n",argv[1][0],isalpha(argv[1][0]));
 
+    // printf("%c miniscule -> %c \n",'A',lower('A'));
+    // printf("%c miniscule -> %c \n",'Z',tolower('Z'));
+
+    //    printf("%s becomes %s\n",argv[1],mirror(argv[1]));
+    //printf("%d\n",atoi("19209"));
+    char str[6];
+    itoa(11021,str);
+    printf("%d becomes %s \n",10021,str);
     return 0;
 }
